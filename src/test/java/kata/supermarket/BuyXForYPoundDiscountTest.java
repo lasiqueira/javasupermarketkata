329efb436d23538f5b11a23dadea4d0bf33fc538
lasiqueira <lucas.asiq@gmail.com>
2020-09-27 15:25:59 +0100

    added corner case test for when the sum of items cost less than the given discount

--- src/test/java/kata/supermarket/BuyXForYPoundDiscountTest.java
+++ src/test/java/kata/supermarket/BuyXForYPoundDiscountTest.java
@@ -21,8 +21,7 @@ public class BuyXForYPoundDiscountTest {
     @DisplayName("Buy One Get One Free Discount provides discounted value...")
     @MethodSource
     @ParameterizedTest(name = "{0}")
-    void discountSchemeProvidesDiscountedValue(String description, String expectedTotal, Iterable<Item> items) {
-        Discount discount = new BuyXForYPoundDiscount("002", 2, BigDecimal.ONE.setScale(2, RoundingMode.HALF_UP));
+    void discountSchemeProvidesDiscountedValue(String description, String expectedTotal, Iterable<Item> items, Discount discount) {
         List<Item> itemList = new ArrayList<>();
         items.forEach(itemList::add);
         assertEquals(new BigDecimal(expectedTotal), discount.getDiscount(itemList));
@@ -33,7 +32,8 @@ public class BuyXForYPoundDiscountTest {
                 noItems(),
                 aSingleItemPricedPerUnit(),
                 multipleItemsPricedPerUnit(),
-                threeItemsWithBuyTwoItemsForOnePoundDiscount()
+                threeItemsWithBuyTwoItemsForOnePoundDiscount(),
+                threeItemsWithBuyTwoItemsForOnePoundDiscountFail()
         );
     }
 
@@ -41,21 +41,29 @@ public class BuyXForYPoundDiscountTest {
 
     private static Arguments multipleItemsPricedPerUnit() {
         return Arguments.of("multiple items priced per unit", "0.00",
-                Arrays.asList(aPackOfDigestives(), aPintOfMilk()));
+                Arrays.asList(aPackOfDigestives(), aPintOfMilk()),
+                buyTwoForOnePoundDiscountDigestive());
     }
 
     private static Arguments aSingleItemPricedPerUnit() {
-        return Arguments.of("a single item priced per unit", "0.00", Collections.singleton(aPintOfMilk()));
+        return Arguments.of("a single item priced per unit", "0.00", Collections.singleton(aPintOfMilk()), buyTwoForOnePoundDiscountDigestive());
     }
 
 
     private static Arguments threeItemsWithBuyTwoItemsForOnePoundDiscount() {
         return Arguments.of("three items with buy two for one pound discount", "2.10",
-                Arrays.asList(aPackOfDigestives(), aPackOfDigestives(), aPackOfDigestives()));
+                Arrays.asList(aPackOfDigestives(), aPackOfDigestives(), aPackOfDigestives()),
+                buyTwoForOnePoundDiscountDigestive());
+    }
+
+    private static Arguments threeItemsWithBuyTwoItemsForOnePoundDiscountFail() {
+        return Arguments.of("three items with buy two for one pound discount fail", "0.00",
+                Arrays.asList(aPintOfMilk(), aPintOfMilk(), aPintOfMilk()),
+        buyTwoForOnePoundDiscountMilk());
     }
 
     private static Arguments noItems() {
-        return Arguments.of("no items", "0.00", Collections.emptyList());
+        return Arguments.of("no items", "0.00", Collections.emptyList(), buyTwoForOnePoundDiscountDigestive());
     }
 
     private static Item aPintOfMilk() {
@@ -65,4 +73,9 @@ public class BuyXForYPoundDiscountTest {
     private static Item aPackOfDigestives() {
         return new Product("002", "Digestives", new BigDecimal("1.55")).oneOf();
     }
+
+    private static Discount buyTwoForOnePoundDiscountDigestive(){ return  new BuyXForYPoundDiscount("002", 2, BigDecimal.ONE.setScale(2, RoundingMode.HALF_UP));}
+    private static Discount buyTwoForOnePoundDiscountMilk(){ return  new BuyXForYPoundDiscount("002", 1, BigDecimal.ONE.setScale(2, RoundingMode.HALF_UP));}
+
+
 }
